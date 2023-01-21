1032783f5e6319ead1749bdc5f236bf202239fc9
ramazanemreosmanoglu <ramazanemre@protonmail.com>
2023-01-21 20:48:44 +0300

    [Basics] Seperate BASE_DIR at the get_asset file
    This reverts commit c0a686c3581dbf2df909fb7abeb0a12c1f697635.

--- foss505/ui/get_asset.py
+++ foss505/ui/get_asset.py
@@ -3,8 +3,7 @@ Simple utility to get the path of the asset file requested.
 """
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-ASSETS_DIR = BASE_DIR / "assets"
+ASSETS_DIR = Path(__file__).resolve().parent / "assets"
 
 def asset(path: str):
     return str(ASSETS_DIR / path)
